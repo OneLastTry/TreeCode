3b25c6392baa6779a9db150b3f139f645f931cca
Stefans Mezulis <stefans.mezulis@gmail.com>
2012-03-21 17:23:59 +0000

    Added command line switch to 3d-plasma-freq for open/closed boundaries

--- TreeCode2/examples/3d-plasma-freq/main.cpp
+++ TreeCode2/examples/3d-plasma-freq/main.cpp
@@ -35,10 +35,10 @@ namespace po = boost::program_options;
 
 void parse_cmd_line(int argc, char **argv,
 		double& timestep, double &theta, double &max_time, double &fs,
-		double& length, unsigned int& num_parts, double& proportion, double& wavelengths, double& temperature){
+		double& length, unsigned int& num_parts, double& proportion, double& wavelengths, double& temperature, bool& is_periodic){
 
 	OptionParser op(argc, argv);
-	bool help;
+	bool help = false, periodic = false, open = false;
 
 	op << new ArgOption<double>("--length", "-l", "Length of system (in debye lengths)", length) <<
 			new ArgOption<double>("--timestep", "-dt", "Individual timestep.", timestep) <<
@@ -49,16 +49,29 @@ void parse_cmd_line(int argc, char **argv,
 			new ArgOption<double>("--proportion", "-prop", "Proportion of particles in sinusoidal perturbation (0.0-1.0).", proportion) <<
 			new ArgOption<double>("--wavelengths", "-w", "Number of wavelengths in perturbation.", wavelengths) <<
 			new ArgOption<double>("--temperature", "-temp", "Temperature of plasma.", temperature) <<
+			new BoolOption("--open", "-o", "Use open boundary conditions", open) <<
+			new BoolOption("--periodic", "-p", "Use periodic boundary conditions", periodic) <<
 			new BoolOption("--help", "-h", "This help text.", help);
 	unsigned int num_options = op.parse();
 
-	if(help || num_options != op.size() - 1){
+	if(!periodic && !open){
+		op.display(std::cerr);
+		exit(1);
+	}else if(periodic && open){
+		std::cerr << "Must specify only one of --periodic or --open." << std::endl;
+		exit(1);
+	}
+	is_periodic = periodic;
+
+	if(help || num_options != op.size() - 2){
 		op.display();
 		exit(0);
 	}
 }
 
-int main(int argc, char **argv) {
+void periodic_start(double length, double wavelengths, double proportion,
+		double temperature, double timestep, double theta, double max_time,
+		double force_softening, unsigned int num_particles){
 	using namespace treecode::distribution;
 	using namespace treecode::potentials;
 	using namespace treecode::pusher;
@@ -67,12 +80,6 @@ int main(int argc, char **argv) {
 	using boost::mt19937;
 	mt19937 rng;
 
-
-	double length, wavelengths, proportion, temperature, timestep, theta, max_time, force_softening;
-	unsigned int num_particles;
-	std::string dbname;
-	parse_cmd_line(argc, argv, timestep, theta, max_time, force_softening,
-			length, num_particles, proportion, wavelengths, temperature);
 
 	Vec origin(-length/2,-length/2,-length/2);
 	Vec max(length/2, length/2, length/2);
@@ -97,10 +104,10 @@ int main(int argc, char **argv) {
 	parts.insert(parts.end(), perturbed_electrons.begin(), perturbed_electrons.end());
 
 	PeriodicBoundary3d		bounds(origin, length);
-	BarnesHutMAC<Vec,Mat>		mac(0.7, bounds);
+	BarnesHutMAC<Vec,Mat>		mac(theta, bounds);
 	CoulombForce3d 			open_pot(force_softening, bounds);
-	EwaldForce3d			periodic_pot(force_softening, bounds, 2.0 / length, 10, 10);
-	InterpolatedEwaldSum3d	potential(force_softening, bounds, 25, periodic_pot, open_pot);
+	EwaldForce3d			periodic_pot(force_softening, bounds, 2.0 / length, 5, 5);
+	InterpolatedEwaldSum3d	potential(force_softening, bounds, 20, periodic_pot, open_pot);
 	potential.init();
 	LeapfrogPusher3d 		push(timestep, bounds, potential);
 	Tree3d					tree(bounds, parts);
@@ -114,6 +121,71 @@ int main(int argc, char **argv) {
 	push.init(parts, tree, quadrupole, mac);
 
 	integrator.start(quadrupole, 1);
+}
+
+void open_start(double length, double wavelengths, double proportion,
+		double temperature, double timestep, double theta, double max_time,
+		double force_softening, unsigned int num_particles){
+	using namespace treecode::distribution;
+	using namespace treecode::potentials;
+	using namespace treecode::pusher;
+	using namespace treecode::output;
+
+	using boost::mt19937;
+	mt19937 rng;
+
+
+	Vec origin = Vec::Zero();
+	SphericalDistribution3d			position_dist(3, origin, length);
+	SphericalDistribution3d			perturbed_dist(3, origin, length/20);
+	ConstDistribution3d				i_velocity_dist(Vec::Zero());
+	MaxwellDistribution3d			e_velocity_dist(1, temperature);
+	ConstantChargeDistribution3d	electron_charges(-1);
+	ConstantChargeDistribution3d	ion_charges(1);
+
+	int id = 0;
+	vector<Particle3d*> parts;
+	vector<Particle3d*> ions = Particle3d::generateParticles<mt19937>(num_particles, 100000, rng,
+			position_dist, i_velocity_dist, ion_charges, id);
+	vector<Particle3d*> electrons = Particle3d::generateParticles<mt19937>(num_particles * (1 - proportion), 1, rng,
+				position_dist, e_velocity_dist, electron_charges, id);
+	vector<Particle3d*> perturbed_electrons = Particle3d::generateParticles<mt19937>(num_particles*proportion, 1, rng,
+					perturbed_dist, e_velocity_dist, electron_charges, id);
+
+	parts.insert(parts.end(), ions.begin(), ions.end());
+	parts.insert(parts.end(), electrons.begin(), electrons.end());
+	parts.insert(parts.end(), perturbed_electrons.begin(), perturbed_electrons.end());
+
+	OpenBoundary3d			bounds;
+	bounds.init(parts);
+	BarnesHutMAC<Vec,Mat>	mac(theta, bounds);
+	CoulombForce3d 			potential(force_softening, bounds);
+	LeapfrogPusher3d 		push(timestep, bounds, potential);
+	Tree3d					tree(bounds, parts);
+	TimeIntegrator3d		integrator(timestep, max_time, parts, tree, bounds, push, mac);
+	integrator.setEnergyOutputFile("energies.csv");
+	integrator.addParticleTracker(new ParticleTracker<Vec,Mat>("positions.csv", parts, ParticleTracker<Vec,Mat>::POSITION));
+	integrator.addParticleTracker(new ParticleTracker<Vec,Mat>("velocities.csv", parts, ParticleTracker<Vec,Mat>::VELOCITY));
+
+	cout << "Initialising pusher" << endl;
+
+	push.init(parts, tree, quadrupole, mac);
+
+	integrator.start(quadrupole, 1);
+}
+
+int main(int argc, char **argv) {
+
+	double length, wavelengths, proportion, temperature, timestep, theta, max_time, force_softening;
+	unsigned int num_particles;
+	bool periodic;
+	parse_cmd_line(argc, argv, timestep, theta, max_time, force_softening,
+			length, num_particles, proportion, wavelengths, temperature, periodic);
+
+	if(periodic)
+		periodic_start(length, wavelengths, proportion, temperature, timestep, theta, max_time, force_softening, num_particles);
+	else
+		open_start(length, wavelengths, proportion, temperature, timestep, theta, max_time, force_softening, num_particles);
 
 	return 0;
 }
--- TreeCode2/tests/TreeTest.cpp
+++ TreeCode2/tests/TreeTest.cpp
@@ -12,13 +12,19 @@
 #include <boost/foreach.hpp>
 #include <boost/random.hpp>
 #include <cmath>
-
+#include <vector>
 #include <Eigen/Dense>
+
 #include <Particle.h>
 #include <distributions/UniformDistribution.h>
 #include <distributions/ConstDistribution.h>
 #include <distributions/ConstantChargeDistribution.h>
-
+#include <Tree.h>
+#include <Node.h>
+#include <bounds/OpenBoundary.h>
+#include <potentials/CoulombForce.h>
+#include <potentials/Potential.h>
+#include <macs/BarnesHutMAC.h>
 
 using namespace std;
 using namespace treecode;
@@ -32,31 +38,34 @@ template<typename V, typename M>
 struct F {
     F(){
     	using boost::random::mt19937;
+    	typedef Particle<V,M> Particle;
     	mt19937 rng;
 
-    	double length, temperature;
-    	unsigned int num_particles;
+    	double length = 1.0;
+    	unsigned int num_particles = 1000;
 
-    	UniformDistribution3d 			position_dist(Vec::Zero(), Vec::Zero().array() + length);
-    	ConstDistribution3d				velocity_dist(Vec::Zero());
-    	ConstantChargeDistribution3d	electron_charges(-1);
-    	ConstantChargeDistribution3d	ion_charges(1);
+    	UniformDistribution<mt19937,V> 			position_dist(V::Zero(), V::Zero().array() + length);
+    	ConstDistribution<mt19937,V>			velocity_dist(V::Zero());
+    	ConstantChargeDistribution<mt19937>		electron_charges(-1);
+    	ConstantChargeDistribution<mt19937>		ion_charges(1);
 
     	int id = 0;
-    	vector<Particle3d*> ions = Particle3d::generateParticles<mt19937>(num_particles, 1837, rng,
+    	vector<Particle*> ions = Particle::template generateParticles<mt19937>(num_particles, 1837, rng,
     			position_dist, velocity_dist, ion_charges, id);
-    	vector<Particle3d*> electrons = Particle3d::generateParticles<mt19937>(num_particles, 1837, rng,
-    	    			position_dist, velocity_dist, ion_charges, id);
+    	vector<Particle*> electrons = Particle::template generateParticles<mt19937>(num_particles, 1, rng,
+    	    			position_dist, velocity_dist, electron_charges, id);
     	parts.insert(parts.end(), ions.begin(), ions.end());
     	parts.insert(parts.end(), electrons.begin(), electrons.end());
-    	OpenBoundary<V,M> bounds();
-    	tree = new Tree()
+    	bounds.init(parts);
+    	tree = new Tree<V,M>(bounds, parts);
+    	tree->rebuild();
     }
 
     ~F(){
     	delete tree;
     }
 
+    OpenBoundary<V,M> bounds;
     Tree<V,M> *tree;
     std::vector<Particle<V,M>* > parts;
 };
@@ -101,6 +110,8 @@ V analytic_force(const Particle<V,M>& test_part, const vector<Particle<V,M>*> pa
 	V force = V::Zero();
 	typedef Particle<V,M> Particle;
 	BOOST_FOREACH(Particle* p, parts){
+		if(p == &test_part)
+			continue;
 		V r = (test_part.getPosition() - p->getPosition());
 		force += p->getCharge() * r / r.squaredNorm() / r.norm();
 	}
@@ -111,15 +122,94 @@ double analytic_potential(const Particle<V,M>& test_part, const vector<Particle<
 	double potential = 0;
 	typedef Particle<V,M> Particle;
 	BOOST_FOREACH(Particle* p, parts){
+		if(p == &test_part)
+			continue;
 		V r = (test_part.getPosition() - p->getPosition());
 		potential += p->getCharge() / r.norm();
 	}
 	return potential;
 }
 
-BOOST_AUTO_TEST_SUITE(Moments);
+template<typename V, typename M>
+V force_on_particle(
+		Particle<V,M>* p,
+		const Tree<V,M>& tree,
+		const potentials::Potential<V, M>& potential,
+		potentials::Precision precision,
+		const AcceptanceCriterion<V, M>& mac
+		) {
+	typedef Node<V, M> Node;
+	typedef std::vector<Node*> interaction_list;
+	V force = V::Zero();
+
+	interaction_list ilist;
+	tree.getInteractionList(*p, ilist, mac);
+	for (typename interaction_list::iterator it = ilist.begin(); it < ilist.end(); it++) {
+		Node* n = *it;
+		force += potential.getForce(*p, *n, precision);
+	}
+	return force;
+}
+
+template<typename V, typename M>
+double potenital_at_particle(
+		Particle<V,M>* p,
+		const Tree<V,M>& tree,
+		const potentials::Potential<V, M>& potential,
+		potentials::Precision precision,
+		const AcceptanceCriterion<V, M>& mac
+		) {
+	typedef Node<V, M> Node;
+	typedef std::vector<Node*> interaction_list;
+	double pot = 0;
+
+	interaction_list ilist;
+	tree.getInteractionList(*p, ilist, mac);
+	for (typename interaction_list::iterator it = ilist.begin(); it < ilist.end(); it++) {
+		Node* n = *it;
+		pot += potential.getPotential(*p, *n, precision);
+	}
+	return pot;
+}
+
+typedef Eigen::Vector2d V2d;
+typedef Eigen::Matrix2d M2d;
+typedef F<V2d, M2d> Fixture2d;
+BOOST_FIXTURE_TEST_SUITE(Moments, Fixture2d)
 
 BOOST_AUTO_TEST_CASE(TwoDMoments){
+	const Node<V2d, M2d>& root = tree->getRoot();
+	int total_charge, modulus_charge;
+	V2d anal_coc  = analytic_centre_of_charge<V2d,M2d>(parts, total_charge, modulus_charge);
+	V2d anal_dip  = analytic_dipole_moment<V2d,M2d>(parts, root.getCentreOfCharge());
+	M2d anal_quad = analytic_quadrupole_moment<V2d,M2d>(parts, root.getCentreOfCharge());
+
+	for(unsigned int i=0;i<anal_coc.rows();i++)
+		BOOST_CHECK_CLOSE(anal_coc[i], root.getCentreOfCharge()[i], TOLERANCE);
+	for(unsigned int i=0;i<anal_dip.rows();i++)
+		BOOST_CHECK_CLOSE(anal_dip[i], root.getDipoleMoments()[i], TOLERANCE);
+	for(unsigned int i=0;i<anal_quad.rows();i++)
+		for(unsigned int j=0;j<anal_quad.cols();j++)
+			BOOST_CHECK_CLOSE(anal_quad.coeff(i,j), root.getQuadrupoleMoments().coeff(i,j), TOLERANCE);
+}
+
+BOOST_AUTO_TEST_CASE(TwoDForces){
+	using namespace potentials;
+	CoulombForceThreeD<V2d,M2d> pot(0.0, bounds);
+	BarnesHutMAC<V2d,M2d> mac(0.0, bounds);
+	Particle<V2d,M2d>* part = parts.front();
+
+	V2d tree_force = force_on_particle<V2d,M2d>(part, *tree, pot, quadrupole, mac);
+	V2d anal_force = analytic_force<V2d,M2d>(*part, parts);
+	double tree_pot = potenital_at_particle<V2d,M2d>(part, *tree, pot, quadrupole, mac);
+	double anal_pot = analytic_potential<V2d,M2d>(*part, parts);
+
+	//Should be identical with theta = 0
+	for(unsigned int i=0;i< tree_force.rows();i++)
+		BOOST_CHECK_CLOSE(anal_force[i], tree_force[i], TOLERANCE);
+	BOOST_CHECK_CLOSE(tree_pot, anal_pot, TOLERANCE);
+
+	//Now try with theta = 0.5
 
 }
 
